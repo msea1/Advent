aeb2d04b67e4358d62966426f24f55e037438fa3
Matthew Carruth <carruthm@gmail.com>
2022-12-03 18:01:50 -0800

    implement day01a

--- python/src/day_01a.py
+++ python/src/day_01a.py
@@ -39,11 +39,37 @@ Find the Elf carrying the most Calories. How many total Calories is that Elf car
 """
 
 def find_most_calories_carried(input_str: str) -> int:
-    pass
+    max_calories_seen = 0
+    if not input_str:
+        return max_calories_seen
+    per_elf_list = turn_input_list_into_list_per_elf(input_str)
+    for elf_load in per_elf_list:
+        elfs_sum = sum(elf_load)
+        max_calories_seen = max(max_calories_seen, elfs_sum)
+    return max_calories_seen
 
+def turn_input_list_into_list_per_elf(input_str: str) -> list[list[int]]:
+    # turn string input into usable data structure
+    # first into a list of strings, one per line, including the extra blanks
+    # e.g. '1 \n 2 \n 3 \n\n 4 \n 5' -> ['1', '2', '3', '', '4', '5']
+    # then coalesce around the extra blanks so that you get one entry per elf
+    # e.g. [ [1, 2, 3], [4, 5] ]
 
+    expanded_list = [i.strip() for i in input_str.splitlines()]
+    condensed_list = [[]]
+    elf_counter = 0
+    for entry in expanded_list:
+        if entry == "":
+            elf_counter += 1
+            condensed_list.append([])
+        else:
+            condensed_list[elf_counter].append(int(entry))
+    return condensed_list
 
 def generate_answer() -> None:
     with open("../inputs/day_01a.txt") as fin:
         input = fin.read()
     print(f"Answer to Day 01a is {find_most_calories_carried(input)}")
+
+
+generate_answer()
